be5d41b78b9558ce9de2a2811f11378b79b8581e
Mozart Rueckert <mozartrue@outlook.com>
2023-11-21 21:45:54 -0300

    feat: set interface to props and add properties

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,11 +1,21 @@
 import { randomUUID } from "crypto";
 
+interface AnswerProps {
+  authorId: string;
+  questionId: string;
+  content: string;
+}
+
 export class Answer {
   public id: string;
+  public instructorId: string;
+  public questionId: string;
   public content: string;
 
-  constructor(content: string, id?: string) {
+  constructor(props: AnswerProps, id?: string) {
     this.id = id ?? randomUUID();
-    this.content = content;
+    this.instructorId = props.authorId;
+    this.questionId = props.questionId;
+    this.content = props.content;
   }
 }
