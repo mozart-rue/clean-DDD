01980c99ffc5456800184a7cf9e9a6ce14f2f987
Mozart Rueckert <mozartrue@outlook.com>
2023-11-22 23:01:24 -0300

    feat: refactor -> removed constructor, changed id type

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,17 +1,14 @@
 import { Entity } from "../../core/entites/entity";
+import { UniqueEntityId } from "../../core/entites/unique-entity-id";
 
 interface AnswerProps {
-  authorId: string;
-  questionId: string;
+  authorId: UniqueEntityId;
+  questionId: UniqueEntityId;
   content: string;
 }
 
 export class Answer extends Entity<AnswerProps> {
   get content() {
     return this.props.content;
-  }
-
-  constructor(props: AnswerProps, id?: string) {
-    super(props, id);
   }
 }
