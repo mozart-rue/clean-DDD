05a7298994bcb5d9765eabb0c7f548d7cdf0b774
Mozart Rueckert <mozartrue@outlook.com>
2023-11-22 23:31:34 -0300

    feat: set static method create to initialize class

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,7 +1,14 @@
 import { Entity } from "../../core/entites/entity";
+import { UniqueEntityId } from "../../core/entites/unique-entity-id";
 
 interface InstructorProps {
   name: string;
 }
 
-export class Instructor extends Entity<InstructorProps> {}
+export class Instructor extends Entity<InstructorProps> {
+  static create(props: InstructorProps, id?: UniqueEntityId) {
+    const instructor = new Instructor(props, id);
+
+    return instructor;
+  }
+}
