0b63854991b1f53f7af6c2b0c028fc554a42ffb8
Mozart Rueckert <mozartrue@outlook.com>
2023-11-22 22:21:06 -0300

    feat: add property slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,14 +1,17 @@
 import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
   authorId: string;
   title: string;
+  slug: Slug;
   content: string;
 }
 
 export class Question {
   public id: string;
   public title: string;
+  public slug: Slug;
   public content: string;
   public authorId: string;
 
@@ -16,6 +19,7 @@ export class Question {
     this.id = id ?? randomUUID();
     this.authorId = props.authorId;
     this.title = props.title;
+    this.slug = props.slug;
     this.content = props.content;
   }
 }
