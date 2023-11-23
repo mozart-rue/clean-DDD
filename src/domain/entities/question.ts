5af053e426a6ede946329d9dbd5bacacb5e338aa
Mozart Rueckert <mozartrue@outlook.com>
2023-11-22 23:04:11 -0300

    feat: add properties

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -4,9 +4,12 @@ import { UniqueEntityId } from "../../core/entites/unique-entity-id";
 
 interface QuestionProps {
   authorId: UniqueEntityId;
+  bestAnswerId?: UniqueEntityId;
   title: string;
   slug: Slug;
   content: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}
