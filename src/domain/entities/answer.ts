11b97b3162a82cf17660f2d51d9fe351fade29e2
Mozart Rueckert <mozartrue@outlook.com>
2023-11-22 23:32:46 -0300

    feat: set static method create to initialize class and add optional params

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,5 +1,6 @@
 import { Entity } from "../../core/entites/entity";
 import { UniqueEntityId } from "../../core/entites/unique-entity-id";
+import { Optional } from "../../core/types/optional";
 
 interface AnswerProps {
   authorId: UniqueEntityId;
@@ -12,5 +13,20 @@ interface AnswerProps {
 export class Answer extends Entity<AnswerProps> {
   get content() {
     return this.props.content;
+  }
+
+  static create(
+    props: Optional<AnswerProps, "createdAt">,
+    id?: UniqueEntityId,
+  ) {
+    const answer = new Answer(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id,
+    );
+
+    return answer;
   }
 }
