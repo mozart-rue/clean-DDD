9b4d563ea82469630d68c583ab50d9ef50173cfb
Mozart Rueckert <mozartrue@outlook.com>
2023-12-13 23:07:14 -0300

    feat: use factory to create a new question

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,7 +1,6 @@
 import { InMemoryQuestionRepository } from "test/respositories/in-memory-question-repository";
 import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
-import { Question } from "../../enterprise/entities/question";
-import { UniqueEntityId } from "@/core/entites/unique-entity-id";
+import { makeQuestion } from "test/factories/make-question";
 import { Slug } from "../../enterprise/entities/value-objects/slug";
 
 let inMemoryQuestionRespository: InMemoryQuestionRepository;
@@ -14,12 +13,7 @@ describe("Get Question By Slug", () => {
   });
 
   it("should be able to get a question by slug", async () => {
-    const newQuestion = Question.create({
-      authorId: new UniqueEntityId(),
-      title: "Example question",
-      slug: Slug.create("example-question"),
-      content: "example content",
-    });
+    const newQuestion = makeQuestion({ slug: Slug.create("example-question") });
 
     await inMemoryQuestionRespository.create(newQuestion);
 
