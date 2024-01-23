57b5d3a556b937d61bcda7672ce4a63ef7855329
Mozart Rueckert <mozartrue@outlook.com>
2024-01-22 22:02:36 -0300

    feat: add optional parameter id

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -5,13 +5,19 @@ import {
   QuestionProps,
 } from "@/domain/forum/enterprise/entities/question";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityId(),
-    title: faker.lorem.sentence(),
-    content: faker.lorem.text(),
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  );
 
   return question;
 }
